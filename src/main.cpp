bff85ad756fcf1cbbea609c1ca1dba9053926ffb
Andrew Bullock <trullock@gmail.com>
2025-01-09 11:58:53 +0000

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,32 @@
 #include <Arduino.h>
 #include <Adafruit_BMP280.h>
-#include <hd44780.h>
-#include <hd44780ioClass/hd44780_I2Cexp.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define BMP_SCK  (13)
-#define BMP_MISO (12)
-#define BMP_MOSI (11)
-#define BMP_CS   (10)
-
-
-hd44780_I2Cexp lcd;
-Adafruit_BMP280 bmp; // I2C
-//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
-//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
+Adafruit_SSD1306 lcd(128, 64, &Wire, -1);
+Adafruit_BMP280 bmp;
 
 void setup() {
 	Serial.begin(9600);
 	Serial.println(F("BMP280 Forced Mode Test."));
 
-	lcd.begin(16, 2);
-	lcd.clear();
-	lcd.setCursor(0, 0);
-	lcd.print("Initializing...");
+	if(!lcd.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+		Serial.println(F("SSD1306 allocation failed"));
+		for(;;);
+	}
+	
+	Serial.println(F("Setting up display"));
 
-	//if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
-	if (!bmp.begin()) {
+	lcd.clearDisplay();
+	lcd.setTextSize(1);
+	lcd.setTextColor(WHITE);
+	lcd.setCursor(0, 10);
+	lcd.print("Initializing...");
+	lcd.display();
+
+	if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
 		Serial.println(F("Could not find a valid BMP280 sensor, check wiring or try a different address!"));
 		while (1) 
 			delay(10);
@@ -36,23 +38,30 @@ void setup() {
 					Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
 					Adafruit_BMP280::FILTER_X16,      /* Filtering. */
 					Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+	
+	lcd.clearDisplay();
 }
 
 void loop() {
 	// must call this to wake sensor up and get new measurement data
 	// it blocks until measurement is complete
 	if (bmp.takeForcedMeasurement()) {
+		lcd.clearDisplay();
+		
+		lcd.setCursor(0, 0);
 		lcd.print(bmp.readTemperature());
-		lcd.println("*C  ");
+		lcd.print("*C  ");
 
 		lcd.print(bmp.readPressure());
-		lcd.println("Pa");
+		lcd.print("Pa");
 
 
-		lcd.setCursor(0, 1);
+		lcd.setCursor(0, 20);
 		lcd.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
-		lcd.println("m");
+		lcd.print("m");
+		lcd.display();
 
+		delay(50);
 	} else {
 		Serial.println("Forced measurement failed!");
 	}
